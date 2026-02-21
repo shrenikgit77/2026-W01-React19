2edd804b6908bef560e3c4ccb5b6808dddfb7e0c
shrenik <shrenik@gmail.com>
2026-02-21 12:16:21 +0530

    Js Challenge

---
+++ Js Overview/script8.js
@@ -0,0 +1,21 @@
+let user = {
+    profile:[
+        {name:"Ashish"},
+        {name:"Varun"},
+        {name:"Vinod"}
+    ],
+
+    rating:[
+        {count:145},
+        {count:189},
+        {count:144}
+    ]
+}
+
+
+//Get the following format of the output
+output = {
+    "Ashish":{rating:145},
+    "Varun":{rating:189},
+    "Vinod":{rating:144}
+}
